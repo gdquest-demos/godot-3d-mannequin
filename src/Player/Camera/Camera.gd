7a050f68798326c1b9624ca79d1856890039fa22
Cheeseness <contact@jbushproductions.com>
2019-09-04 17:26:59 +1000

    More movement and camera control work * Moved camera script to camera anchor * Moved camera handling code to camera script * Made camera anchor top level * Added spyro-like automatic camera rotation

--- src/Player/Camera/Camera.gd
+++ src/Player/Camera/Camera.gd
@@ -1,7 +1,76 @@
-extends Camera
+extends Spatial
 
 var _target: Spatial
+export var camera_rotation_speed: = Vector2(2.5, 2.5)
+export var yinvert_multiplier: = -1
+export var camera_backwards_deadzone := 0.3
+var _camera_rotating: = false
+
+onready var rotation_delay: Timer = $RotateDelay
+
 
 func _ready() -> void:
 	_target = owner
-	#set_as_toplevel(true)
+	set_as_toplevel(true)
+
+func physics_process(delta: float, move_direction: Vector3) -> void:
+	#TODO: Remove move direction visual aid - or turn it into a gizmo?
+	get_node("MoveDirectionRef").set_translation(move_direction)
+
+	#Snap the camera to the player position
+	var pos = get_global_transform()
+	pos.origin = owner.get_global_transform().origin
+	set_global_transform(pos)
+
+	var camera_direction: = get_camera_direction()
+	if camera_direction.length() == 0:
+		#Handle automatic camera rotation
+		#TODO: Automatically reset camera vertical rotation over time
+		if move_direction.z > 0:
+			#If the player is moving directly towards the camera, don't rotate
+			if move_direction.x <= -camera_backwards_deadzone || move_direction.x >= camera_backwards_deadzone:
+				if !_camera_rotating:
+					rotation_delay.start()
+					_camera_rotating = true
+				if rotation_delay.time_left <= 0.0:
+					var targetAngle: float = owner.rotation.y
+					var cameraOffset: float = owner.rotation.y - rotation.y
+					#Unwind rotation in case we're outside the domain that player rotation works within
+					if cameraOffset > PI:
+							targetAngle -= 2 * PI
+					elif cameraOffset < - PI:
+							targetAngle += 2 * PI
+					rotation.y = lerp(rotation.y, targetAngle, 0.015)
+			else:
+				_camera_rotating = false
+		else:
+			_camera_rotating = false
+	else:
+		_camera_rotating = false
+
+		#Apply manual camera input
+		if camera_direction.length() > 1:
+			camera_direction = camera_direction.normalized()
+		if camera_direction.x != 0:
+			rotation.y -= camera_direction.x * camera_rotation_speed.x * delta
+		if camera_direction.y != 0:
+			rotation.x -= camera_direction.y * camera_rotation_speed.y * delta * yinvert_multiplier
+			rotation.x = clamp(rotation.x, -0.75, 0.9)
+		#TODO: Move camera node along local -Z until no occluding geo is in the way
+		#TODO: Make occluding geo transparent?
+
+	#Make sure we don't end up winding
+	if rotation.y > PI:
+		rotation.y -= 2 * PI
+	if rotation.y < -PI:
+		rotation.y += 2 * PI
+
+	if _camera_rotating == false:
+		rotation_delay.stop()
+
+
+static func get_camera_direction() -> Vector2:
+	return Vector2(
+			Input.get_action_strength("look_right") - Input.get_action_strength("look_left"),
+			Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
+		)
--- src/Player/States/Move.gd
+++ src/Player/States/Move.gd
@@ -3,62 +3,34 @@ extends State
 export var max_speed: = Vector3(50.0, 50.0, 500.0)
 export var move_speed: = Vector3(500, 500, 500)
 export var max_rotation_speed: = 0.5
-export var camera_rotation_speed: = Vector2(2.5, 2.5)
-export var yinvert_multiplier: = -1
 
 var velocity: = Vector3.ZERO
+onready var _camera: Spatial = owner.get_node("CameraAnchor")
+
 
 func physics_process(delta: float) -> void:
-	#TODO:Move camera rotation out to Camera.gd
-	var prev_camera_rotation: Vector3 = owner.get_node("CameraAnchor").global_transform.basis.get_euler()
+	var input_offset: float = _camera.global_transform.basis.get_euler().y
 	var move_direction: = get_move_direction()
+
 	#Use Vector2 to calculate angle since Vector3.angle_to() returns angular distance/angular diameter rather than the angle itself
-	var temp: = Vector2(move_direction.x, move_direction.z)
-	var rotation: = temp.angle_to(Vector2.UP)
+	var move_direction_2d: = Vector2(move_direction.x, move_direction.z)
+	var rotation: = move_direction_2d.angle_to(Vector2.UP)
 
-	#TODO: Remove move direction visual aid - or turn it into a gizmo?
-	owner.get_node("CameraAnchor/MoveDirectionRef").set_translation(move_direction)
-
-	var rotdiff: = 0.0
-	if temp.length() > 0:
-		#Keep track of the new rotation amount to use when calculating manual camera control
-		rotdiff = owner.rotation.y - (prev_camera_rotation.y + rotation)
-		owner.rotation.y = prev_camera_rotation.y + rotation
+	if move_direction_2d.length() > 0:
+		owner.rotation.y = input_offset + rotation
 		#Make sure we don't end up winding
 		if owner.rotation.y > PI:
 			owner.rotation.y -= 2 * PI
 		if owner.rotation.y < -PI:
 			owner.rotation.y += 2 * PI
 
-	velocity = calculate_velocity(velocity, max_speed, move_speed, delta, move_direction.rotated(Vector3.UP, (prev_camera_rotation.y)))
+	velocity = calculate_velocity(velocity, max_speed, move_speed, delta, move_direction.rotated(Vector3.UP, (input_offset)))
 	if velocity.y == 0:
 		velocity.y = -0.01
-
 	owner.move_and_slide(velocity, Vector3.UP)
 
-	var camera_direction: = get_camera_direction()
-	if camera_direction.length() == 0:
-		#TODO: Delayed follow behaviour
-		if move_direction.length() > 0:
-			owner.get_node("CameraAnchor").rotation.y = 0 - rotation
-		pass
-	else:
-		#Apply manual camera
-		if camera_direction.length() > 1:
-			camera_direction = camera_direction.normalized()
-		if camera_direction.x != 0:
-			owner.get_node("CameraAnchor").rotation.y -= camera_direction.x * camera_rotation_speed.x * delta - rotdiff
-		if camera_direction.y != 0:
-			owner.get_node("CameraAnchor").rotation.x -= camera_direction.y * camera_rotation_speed.y * delta * yinvert_multiplier
-			owner.get_node("CameraAnchor").rotation.x = clamp(owner.get_node("CameraAnchor").rotation.x, -0.75, 0.9)
-		#TODO: Move camera node along local -Z until no occluding geo is in the way
-		#TODO: Make occluding geo transparent?
+	_camera.physics_process(delta, move_direction)
 
-	#Make sure we don't end up winding
-	if owner.get_node("CameraAnchor").rotation.y > PI:
-		owner.get_node("CameraAnchor").rotation.y -= 2 * PI
-	if owner.get_node("CameraAnchor").rotation.y < -PI:
-		owner.get_node("CameraAnchor").rotation.y += 2 * PI
 
 static func get_move_direction() -> Vector3:
 	return Vector3(
@@ -67,11 +39,6 @@ static func get_move_direction() -> Vector3:
 			Input.get_action_strength("move_back") - Input.get_action_strength("move_front")
 		)
 
-static func get_camera_direction() -> Vector2:
-	return Vector2(
-			Input.get_action_strength("look_right") - Input.get_action_strength("look_left"),
-			Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
-		)
 
 static func calculate_velocity(
 		old_velocity: Vector3,
