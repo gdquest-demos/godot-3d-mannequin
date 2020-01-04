8fe851c600d268f1f701937ad85e9a1c8e23dfd3
Nathan Lovato <nathan@gdquest.com>
2020-01-04 14:57:07 +0100

    Add input (F11) to toggle fullscreen

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -15,3 +15,7 @@ func _input(event: InputEvent) -> void:
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		get_tree().set_input_as_handled()
+
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+		get_tree().set_input_as_handled()
