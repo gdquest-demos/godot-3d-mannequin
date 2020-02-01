14670cc372fd3cebfb74ecfeec07d03e25d6213f
Nathan Lovato <nathan@gdquest.com>
2020-02-01 13:12:25 -0600

    Fix memory leak with the State class in Godot 3.2
    Also updated the docstrings and left a comment
    
    Closes #30

--- src/Main/StateMachine/State.gd
+++ src/Main/StateMachine/State.gd
@@ -1,19 +1,24 @@
 extends Node
 class_name State
-# State interface to use in Hierarchical State Machines.
-# The lowest leaf tries to handle callbacks, and if it can't, it delegates the work to its parent.
-# It's up to the user to call the parent state's functions, e.g. `_parent.physics_process(delta)`
+# State interface to use in Hierarchical State Machines. The lowest leaf tries
+# to handle callbacks, and if it can't, it delegates the work to its parent.
+#
+# It's up to the user to call the parent state's functions, e.g.
+# `_parent.physics_process(delta)`
+#
 # Use State as a child of a StateMachine node.
 
 
 onready var _state_machine: = _get_state_machine(self)
 
-var _parent: State = null
+# Using the same class, i.e. State, as a type hint causes a memory leak in Godot
+# 3.2.
+var _parent = null
 
 
 func _ready() -> void:
 	yield(owner, "ready")
-	var parent: = get_parent()
+	var parent = get_parent()
 	if not parent.is_in_group("state_machine"):
 		_parent = parent
 
