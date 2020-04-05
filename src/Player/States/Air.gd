b0f3d0c25642730cddf8594457c520c762115ff2
Mayhew Steyn <mayhrunes@gmail.com>
2020-04-05 02:05:18 +0200

    Fix unwanted character sliding on slopes and add new ramp obstical
    The character used to slide down slopes with a low enough incline to be
    the floor.
    
    The character used to run up (or slide up if in the Air state) slopes
    that are too steep to be the floor.
    
    New bad behaviour:
    - On slopes with an incline equal to max_floor_angle, the character
    makes a slight hop after running up and looses their grounding when
    running down them.
    - When the character runs into a steep slope they perform short hops due
    to sliding upwards. Ideally the player would stay on the ground (no
    sliding up the slope). This can be exploited to jump higher.
    
    A new ramp obstical (Rampy) was added to the main Game scene to test the
    slope behaviour.
    
    Closes: #25

--- src/Player/States/Air.gd
+++ src/Player/States/Air.gd
@@ -3,12 +3,16 @@ extends PlayerState
 
 
 func physics_process(delta: float) -> void:
+	var velocity_previous_y = _parent.velocity.y
 	_parent.physics_process(delta)
 
 	if player.is_on_floor():
 		_state_machine.transition_to("Move/Idle")
 	elif player.is_on_ceiling():
 		_parent.velocity.y = 0
+	elif player.is_on_wall() and _parent.velocity.y > velocity_previous_y:
+		_parent.velocity.y = velocity_previous_y + _parent.gravity*delta
+	
 
 
 func enter(msg: Dictionary = {}) -> void:
@@ -16,8 +20,10 @@ func enter(msg: Dictionary = {}) -> void:
 		{"velocity": var v, "jump_impulse": var ji}:
 			_parent.velocity = v + Vector3(0, ji, 0)
 	skin.transition_to(skin.States.AIR)
+	_parent.snap = Vector3.ZERO
 	_parent.enter()
 
 
 func exit() -> void:
+	_parent.snap = Vector3.DOWN
 	_parent.exit()
